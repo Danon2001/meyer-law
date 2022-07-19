a649498e7b18bd8e144506282f8f1bbe4c96efa0
DEWNS <bodiavanda@gmail.com>
2022-07-19 15:58:31 +0300

    Реализована страница Команда, Адаптивный футер без бутстрапа

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'Roboto': ['Roboto', 'sans-serif'],
+      'Raleway': ['Raleway', 'sans-serif'],
+      'OpenSans': ['Open Sans', 'sans-serif'],
+      'PTSerif': ['PT Serif', 'serif'],
+      "Helvetica": ['Helvetica,Arial','sans-serif']
+    },
   },
   plugins: [],
 }
